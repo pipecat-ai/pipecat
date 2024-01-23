4cd9993a5e7fc3481400f199fd6a9c469cd19584
Moishe Lettvin <moishel@gmail.com>
2024-01-23 09:37:15 -0500

    remove commented-out code

--- src/dailyai/queue_frame.py
+++ src/dailyai/queue_frame.py
@@ -2,23 +2,6 @@ from enum import Enum
 from dataclasses import dataclass
 from typing import Any
 
-"""
-class FrameType(Enum):
-    NOOP = -1
-    START_STREAM = 0
-    END_STREAM = 1
-    AUDIO = 2
-    IMAGE = 3
-    TEXT = 4
-    TRANSCRIPTION = 5
-    LLM_MESSAGE = 6
-    APP_MESSAGE = 7
-
-@dataclass(frozen=True)
-class QueueFrame:
-    frame_type: FrameType
-    frame_data: str | dict | bytes | list | None
-"""
 class QueueFrame:
     pass
 
