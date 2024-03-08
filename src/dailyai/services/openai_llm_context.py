226f6966386aae43f8e2045e4e74287c7fa91f1c
Moishe Lettvin <moishel@gmail.com>
2024-03-08 11:09:16 -0500

    Base OpenAI LLM service

---
+++ src/dailyai/services/openai_llm_context.py
@@ -0,0 +1,52 @@
+from typing import List
+from openai._types import NOT_GIVEN, NotGiven
+
+from openai.types.chat import (
+    ChatCompletionToolParam,
+    ChatCompletionToolChoiceOptionParam,
+    ChatCompletionMessageParam,
+)
+
+
+class OpenAILLMContext:
+
+    def __init__(
+        self,
+        messages: List[ChatCompletionMessageParam] | None = None,
+        tools: List[ChatCompletionToolParam] | NotGiven = NOT_GIVEN,
+        tool_choice: ChatCompletionToolChoiceOptionParam | NotGiven = NOT_GIVEN
+    ):
+        self.messages: List[ChatCompletionMessageParam] = messages if messages else []
+        self.tool_choice: ChatCompletionToolChoiceOptionParam | NotGiven = tool_choice
+        self.tools: List[ChatCompletionToolParam] | NotGiven = tools
+
+    @staticmethod
+    def from_messages(messages: List[dict]) -> "OpenAILLMContext":
+        context = OpenAILLMContext()
+        for message in messages:
+            context.add_message({
+                "content":message["content"],
+                "role":message["role"],
+                "name":message["name"] if "name" in message else message["role"]
+            })
+        return context
+
+    #def __deepcopy__(self, memo):
+
+    def add_message(self, message: ChatCompletionMessageParam):
+        self.messages.append(message)
+
+    def get_messages(self) -> List[ChatCompletionMessageParam]:
+        return self.messages
+
+    def set_tool_choice(
+        self, tool_choice: ChatCompletionToolChoiceOptionParam | NotGiven
+    ):
+        self.tool_choice = tool_choice
+
+    def set_tools(self, tools:List[ChatCompletionToolParam] | NotGiven = NOT_GIVEN):
+        if tools != NOT_GIVEN and len(tools) == 0:
+            tools = NOT_GIVEN
+
+        self.tools = tools
+
---
+++ src/dailyai/tests/integration/integration_openai_llm.py
@@ -0,0 +1,84 @@
+import asyncio
+import os
+from dailyai.pipeline.frames import (
+    OpenAILLMContextFrame,
+)
+from dailyai.services.openai_llm_context import OpenAILLMContext
+
+from openai.types.chat import (
+    ChatCompletionSystemMessageParam,
+    ChatCompletionToolParam,
+    ChatCompletionUserMessageParam,
+)
+
+from dailyai.services.openai_api_llm_service import BaseOpenAILLMService
+
+if __name__ == "__main__":
+    async def test_functions():
+        tools = [
+            ChatCompletionToolParam(
+                type="function",
+                function= {
+                    "name": "get_current_weather",
+                    "description": "Get the current weather",
+                    "parameters": {
+                        "type": "object",
+                        "properties": {
+                            "location": {
+                                "type": "string",
+                                "description": "The city and state, e.g. San Francisco, CA",
+                            },
+                            "format": {
+                                "type": "string",
+                                "enum": ["celsius", "fahrenheit"],
+                                "description": "The temperature unit to use. Infer this from the users location.",
+                            },
+                        },
+                        "required": ["location", "format"],
+                    },
+                }
+            )
+        ]
+
+        api_key = os.getenv("OPENAI_API_KEY")
+
+        llm = BaseOpenAILLMService(
+            api_key=api_key or "",
+            model="gpt-4-1106-preview",
+        )
+        context = OpenAILLMContext(tools=tools)
+        system_message: ChatCompletionSystemMessageParam = ChatCompletionSystemMessageParam(
+            content="Ask the user to ask for a weather report", name="system", role="system"
+        )
+        user_message: ChatCompletionUserMessageParam = ChatCompletionUserMessageParam(
+            content="Could you tell me the weather for Boulder, Colorado",
+            name="user",
+            role="user",
+        )
+        context.add_message(system_message)
+        context.add_message(user_message)
+        frame = OpenAILLMContextFrame(context)
+        async for s in llm.process_frame(frame):
+            print(s)
+
+    async def test_chat():
+        api_key = os.getenv("OPENAI_API_KEY")
+
+        llm = BaseOpenAILLMService(
+            api_key=api_key or "",
+            model="gpt-4-1106-preview",
+        )
+        context = OpenAILLMContext()
+        message: ChatCompletionSystemMessageParam = ChatCompletionSystemMessageParam(
+            content="Please tell the world hello.", name="system", role="system"
+        )
+        context.add_message(message)
+        frame = OpenAILLMContextFrame(context)
+        async for s in llm.process_frame(frame):
+            print(s)
+
+    async def run_tests():
+        await test_functions()
+        await test_chat()
+
+    asyncio.run(run_tests())
