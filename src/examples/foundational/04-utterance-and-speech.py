b8b35db89c8547740b9bfc16e860184175fb9060
Moishe Lettvin <moishel@gmail.com>
2024-03-15 11:04:22 -0400

    Remove run_to_queue and run from AIService class

--- src/examples/foundational/04-utterance-and-speech.py
+++ src/examples/foundational/04-utterance-and-speech.py
@@ -3,12 +3,13 @@ import logging
 import os
 
 import aiohttp
+from dailyai.pipeline.merge_pipeline import SequentialMergePipeline
 from dailyai.pipeline.pipeline import Pipeline
 
 from dailyai.services.daily_transport_service import DailyTransportService
 from dailyai.services.azure_ai_services import AzureLLMService, AzureTTSService
 from dailyai.services.deepgram_ai_services import DeepgramTTSService
-from dailyai.pipeline.frames import EndFrame, LLMMessagesQueueFrame
+from dailyai.pipeline.frames import EndFrame, EndPipeFrame, LLMMessagesQueueFrame, TextFrame
 from dailyai.services.elevenlabs_ai_service import ElevenLabsTTSService
 from examples.support.runner import configure
 
@@ -53,49 +54,24 @@ async def main(room_url: str):
         # Start a task to run the LLM to create a joke, and convert the LLM output to audio frames. This task
         # will run in parallel with generating and speaking the audio for static text, so there's no delay to
         # speak the LLM response.
-        buffer_queue = asyncio.Queue()
-        source_queue = asyncio.Queue()
-        pipeline = Pipeline(
-            source=source_queue, sink=buffer_queue, processors=[llm, elevenlabs_tts]
+        llm_pipeline = Pipeline([llm, elevenlabs_tts])
+        await llm_pipeline.queue_frames([LLMMessagesQueueFrame(messages), EndPipeFrame()])
+
+        simple_tts_pipeline = Pipeline([azure_tts])
+        await simple_tts_pipeline.queue_frames(
+            [
+                TextFrame("My friend the LLM is going to tell a joke about llamas"),
+                EndPipeFrame(),
+            ]
         )
-        await source_queue.put(LLMMessagesQueueFrame(messages))
-        await source_queue.put(EndFrame())
-        pipeline_run_task = pipeline.run_pipeline()
 
-        other_participant_joined = asyncio.Event()
+        merge_pipeline = SequentialMergePipeline([simple_tts_pipeline, llm_pipeline])
 
-        @transport.event_handler("on_first_other_participant_joined")
-        async def on_first_other_participant_joined(transport):
-            other_participant_joined.set()
-
-        async def say_something():
-            await other_participant_joined.wait()
-
-            await azure_tts.say(
-                "My friend the LLM is now going to tell a joke about llamas.",
-                transport.send_queue,
-            )
-
-            # khk: deepgram_tts.say() doesn't seem to put bytes in the transport
-            # queue. I get a debug log line that indicates we're set up okay, but
-            # no further log lines or audio bytes. debug this later:
-            #   20 2024-03-10 13:24:46,235 Running deepgram tts for My friend the LLM is now going to tell a joke about llamas.
-            # await deepgram_tts.say(
-            #    "My friend the LLM is now going to tell a joke about llamas.",
-            #    transport.send_queue,
-            # )
-
-            async def buffer_to_send_queue():
-                while True:
-                    frame = await buffer_queue.get()
-                    await transport.send_queue.put(frame)
-                    buffer_queue.task_done()
-                    if isinstance(frame, EndFrame):
-                        break
-
-            await asyncio.gather(pipeline_run_task, buffer_to_send_queue())
-
-        await asyncio.gather(transport.run(), say_something())
+        await asyncio.gather(
+            transport.run(merge_pipeline),
+            simple_tts_pipeline.run_pipeline(),
+            llm_pipeline.run_pipeline(),
+        )
 
 
 if __name__ == "__main__":
