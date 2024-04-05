9590cc2fbc0d5ad6a784e2a3b743005eb7beeb17
Aleix Conchillo Flaqué <aleix@daily.co>
2024-04-05 13:34:34 -0700

    examples: fix whisper examples

--- examples/foundational/13-whisper-transcription.py
+++ examples/foundational/13-whisper-transcription.py
@@ -1,11 +1,15 @@
 import asyncio
 import logging
 
+from dailyai.pipeline.frames import EndFrame, TranscriptionFrame
 from dailyai.transports.daily_transport import DailyTransport
 from dailyai.services.whisper_ai_services import WhisperSTTService
 from dailyai.pipeline.pipeline import Pipeline
 
 from runner import configure
+
+from dotenv import load_dotenv
+load_dotenv(override=True)
 
 logging.basicConfig(format=f"%(levelno)s %(asctime)s %(message)s")
 logger = logging.getLogger("dailyai")
@@ -26,17 +30,27 @@ async def main(room_url: str):
     stt = WhisperSTTService()
 
     transcription_output_queue = asyncio.Queue()
+    transport_done = asyncio.Event()
 
-    pipeline = Pipeline([stt])
-    pipeline.set_sink(transcription_output_queue)
+    pipeline = Pipeline([stt], source=transport.receive_queue, sink=transcription_output_queue)
 
     async def handle_transcription():
         print("`````````TRANSCRIPTION`````````")
-        while True:
+        while not transport_done.is_set():
             item = await transcription_output_queue.get()
-            print(item.text)
+            print("got item from queue", item)
+            if isinstance(item, TranscriptionFrame):
+                print(item.text)
+            elif isinstance(item, EndFrame):
+                break
+        print("handle_transcription done")
 
-    await asyncio.gather(transport.run(pipeline), handle_transcription())
+    async def run_until_done():
+        await transport.run()
+        transport_done.set()
+        print("run_until_done done")
+
+    await asyncio.gather(run_until_done(), pipeline.run_pipeline(), handle_transcription())
 
 
 if __name__ == "__main__":
