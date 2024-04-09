9fcbb36997ca185ea1924208a2c59a12e1f40ade
Aleix Conchillo Flaqué <aleix@daily.co>
2024-04-09 14:58:06 -0700

    examples: add 14a-local-render-remote-participant

--- examples/foundational/14a-local-render-remote-participant.py
+++ examples/foundational/14a-local-render-remote-participant.py
@@ -1,6 +1,7 @@
 import asyncio
 import io
 import logging
+import tkinter as tk
 
 from typing import AsyncGenerator
 
@@ -10,6 +11,7 @@ from dailyai.pipeline.frames import ImageFrame, Frame, UserImageFrame
 from dailyai.pipeline.pipeline import Pipeline
 from dailyai.transports.daily_transport import DailyTransport
 
+from dailyai.transports.local_transport import LocalTransport
 from runner import configure
 
 from dotenv import load_dotenv
@@ -30,23 +32,40 @@ class UserImageProcessor(FrameProcessor):
 
 
 async def main(room_url: str, token):
+    tk_root = tk.Tk()
+    tk_root.title("dailyai")
+
+    local_transport = LocalTransport(
+        tk_root=tk_root,
+        camera_enabled=True,
+        camera_width=1280,
+        camera_height=720
+    )
+
     transport = DailyTransport(
         room_url,
         token,
         "Render participant video",
-        camera_width=1280,
-        camera_height=720,
-        camera_enabled=True,
         video_rendering_enabled=True
     )
 
-    @ transport.event_handler("on_first_other_participant_joined")
+    @transport.event_handler("on_first_other_participant_joined")
     async def on_first_other_participant_joined(transport, participant):
         transport.render_participant_video(participant["id"])
 
-    pipeline = Pipeline([UserImageProcessor()])
+    async def run_tk():
+        while not transport._stop_threads.is_set():
+            tk_root.update()
+            tk_root.update_idletasks()
+            await asyncio.sleep(0.1)
 
-    await asyncio.gather(transport.run(pipeline))
+    local_pipeline = Pipeline([UserImageProcessor()], source=transport.receive_queue)
+
+    await asyncio.gather(
+        transport.run(),
+        local_transport.run(local_pipeline, override_pipeline_source_queue=False),
+        run_tk()
+    )
 
 if __name__ == "__main__":
     (url, token) = configure()
