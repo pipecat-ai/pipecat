bfe2e0f36eb37d7a53808841a04ded40941ade77
Aleix Conchillo Flaqué <aleix@daily.co>
2024-04-10 09:44:42 -0700

    services: don't use image_size in ImageGenService

--- src/dailyai/services/fal_ai_services.py
+++ src/dailyai/services/fal_ai_services.py
@@ -45,7 +45,7 @@ class FalImageGenService(ImageGenService):
         if key_secret:
             os.environ["FAL_KEY_SECRET"] = key_secret
 
-    async def run_image_gen(self, prompt) -> tuple[str, bytes]:
+    async def run_image_gen(self, prompt: str) -> tuple[str, bytes, tuple[int, int]]:
         def get_image_url(prompt):
             handler = fal.apps.submit(  # type: ignore
                 self._model,
@@ -72,4 +72,4 @@ class FalImageGenService(ImageGenService):
         async with self._aiohttp_session.get(image_url) as response:
             image_stream = io.BytesIO(await response.content.read())
             image = Image.open(image_stream)
-            return (image_url, image.tobytes())
+            return (image_url, image.tobytes(), image.size)
