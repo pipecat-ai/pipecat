3e514c62df9ee307882355c47d60d75dc5db8c14
Aleix Conchillo Flaqué <aleix@daily.co>
2024-05-14 19:06:02 -0700

    examples: simplify 09-mirror.py

--- examples/foundational/09-mirror.py
+++ examples/foundational/09-mirror.py
@@ -7,12 +7,9 @@
 import asyncio
 import sys
 
-from pipecat.frames.frames import AudioRawFrame, ImageRawFrame
-from pipecat.processors.filter import Filter
 from pipecat.pipeline.pipeline import Pipeline
 from pipecat.pipeline.runner import PipelineRunner
 from pipecat.pipeline.task import PipelineTask
-from pipecat.pipeline.parallel_pipeline import ParallelPipeline
 from pipecat.transports.services.daily import DailyTransport, DailyParams
 
 from runner import configure
@@ -23,15 +20,15 @@ from dotenv import load_dotenv
 load_dotenv(override=True)
 
 logger.remove(0)
-logger.add(sys.stderr, level="DEBUG")
+logger.add(sys.stderr, level="TRACE")
 
 
 async def main(room_url, token):
     transport = DailyTransport(
         room_url, token, "Test",
         DailyParams(
-            audio_in_enabled=True,
-            audio_out_enabled=True,
+            #            audio_in_enabled=True,
+            #            audio_out_enabled=True,
             camera_out_enabled=True,
             camera_out_width=1280,
             camera_out_height=720
@@ -42,13 +39,7 @@ async def main(room_url, token):
     async def on_first_participant_joined(transport, participant):
         transport.capture_participant_video(participant["id"])
 
-    # The ParallelPipeline is not really necessary here but it shows how you
-    # would process audio and video concurrently in parallel pipelines.
-    pipeline = Pipeline([transport.input(),
-                         ParallelPipeline(
-                             [Filter([AudioRawFrame])],
-                             [Filter([ImageRawFrame])]),
-                         transport.output()])
+    pipeline = Pipeline([transport.input(), transport.output()])
 
     runner = PipelineRunner()
 
