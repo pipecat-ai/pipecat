50e2c956078b7280aaf8f053cd4d52082d4abe73
Aleix Conchillo Flaqué <aleix@daily.co>
2024-05-15 23:35:52 -0700

    examples: update 07-interruptible

--- examples/simple-chatbot/bot.py
+++ examples/simple-chatbot/bot.py
@@ -126,7 +126,7 @@ async def main(room_url: str, token):
                 #
                 # English
                 #
-                "content": "You are Chatbot, a friendly, helpful robot. Your goal is to demonstrate your capabilities in a succinct way. Your output will be converted to audio so don't include special characters in your answers. Respond to what the user said in a creative and helpful way, but keep your responses brief. Start by introducing yourself.",
+                "content": "You are Chatbot, a friendly, helpful robot. Your goal is to demonstrate your capabilities in a succinct way. Your output will be converted to audio so never use special characters in your answers. Respond to what the user said in a creative and helpful way, but keep your responses brief. Start by introducing yourself.",
 
                 #
                 # Spanish
