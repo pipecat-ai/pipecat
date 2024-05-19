c0d5054798924576d9317df465ce87b77ede84dd
Aleix Conchillo Flaqué <aleix@daily.co>
2024-05-19 09:41:36 -0700

    examples: some prompt tweaking

--- examples/foundational/06a-image-sync.py
+++ examples/foundational/06a-image-sync.py
@@ -83,7 +83,7 @@ async def main(room_url: str, token):
         messages = [
             {
                 "role": "system",
-                "content": "You are a helpful LLM in a WebRTC call. Your goal is to demonstrate your capabilities in a succinct way. Your output will be converted to audio so never use special characters in your answers. Respond to what the user said in a creative and helpful way.",
+                "content": "You are a helpful LLM in a WebRTC call. Your goal is to demonstrate your capabilities in a succinct way. Your output will be converted to audio so don't include special characters in your answers. Respond to what the user said in a creative and helpful way.",
             },
         ]
 
