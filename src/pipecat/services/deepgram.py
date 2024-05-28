546ec4832ec551ac27106e0e83c660cafdea8f56
Kwindla Hultman Kramer <kwindla@gmail.com>
2024-05-28 15:59:26 -0700

    moving Deepgram TTS base_url from beta to prod

--- src/pipecat/services/deepgram.py
+++ src/pipecat/services/deepgram.py
@@ -8,7 +8,7 @@ import aiohttp
 
 from typing import AsyncGenerator
 
-from pipecat.frames.frames import AudioRawFrame, Frame
+from pipecat.frames.frames import AudioRawFrame, ErrorFrame, Frame
 from pipecat.services.ai_services import TTSService
 
 from loguru import logger
@@ -21,7 +21,7 @@ class DeepgramTTSService(TTSService):
             *,
             aiohttp_session: aiohttp.ClientSession,
             api_key: str,
-            voice: str = "alpha-asteria-en-v2",
+            voice: str = "aura-helios-en",
             **kwargs):
         super().__init__(**kwargs)
 
@@ -31,11 +31,18 @@ class DeepgramTTSService(TTSService):
 
     async def run_tts(self, text: str) -> AsyncGenerator[Frame, None]:
         logger.info(f"Running Deepgram TTS for {text}")
-        base_url = "https://api.beta.deepgram.com/v1/speak"
+        base_url = "https://api.deepgram.com/v1/speak"
         request_url = f"{base_url}?model={self._voice}&encoding=linear16&container=none&sample_rate=16000"
         headers = {"authorization": f"token {self._api_key}"}
         body = {"text": text}
+
         async with self._aiohttp_session.post(request_url, headers=headers, json=body) as r:
+            if r.status != 200:
+                text = await r.text()
+                logger.error(f"Error getting audio (status: {r.status}, error: {text})")
+                yield ErrorFrame(f"Error getting audio (status: {r.status}, error: {text})")
+                return
+
             async for data in r.content:
                 frame = AudioRawFrame(audio=data, sample_rate=16000, num_channels=1)
                 yield frame
