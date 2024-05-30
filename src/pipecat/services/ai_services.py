8f925361d882f498c66affb0ad1b374d95dafc41
Aleix Conchillo Flaqué <aleix@daily.co>
2024-05-30 10:54:21 -0700

    services: move function calling registration to LLMService

--- src/pipecat/services/ai_services.py
+++ src/pipecat/services/ai_services.py
@@ -43,6 +43,31 @@ class LLMService(AIService):
 
     def __init__(self):
         super().__init__()
+        self._callbacks = {}
+        self._start_callbacks = {}
+
+    # TODO-CB: callback function type
+    def register_function(self, function_name: str, callback, start_callback=None):
+        self._callbacks[function_name] = callback
+        if start_callback:
+            self._start_callbacks[function_name] = start_callback
+
+    def unregister_function(self, function_name: str):
+        del self._callbacks[function_name]
+        if self._start_callbacks[function_name]:
+            del self._start_callbacks[function_name]
+
+    def has_function(self, function_name: str):
+        return function_name in self._callbacks.keys()
+
+    async def call_function(self, function_name: str, args):
+        if function_name in self._callbacks.keys():
+            return await self._callbacks[function_name](self, args)
+        return None
+
+    async def call_start_function(self, function_name: str):
+        if function_name in self._start_callbacks.keys():
+            await self._start_callbacks[function_name](self)
 
 
 class TTSService(AIService):
