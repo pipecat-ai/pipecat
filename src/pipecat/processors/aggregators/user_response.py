b856979879beabe9bb2d59e0dc9749b191d00016
Aleix Conchillo Flaqué <aleix@daily.co>
2024-06-13 13:30:31 -0700

    aggregator(user_response): also handle small VADParams.stop_secs

--- src/pipecat/processors/aggregators/user_response.py
+++ src/pipecat/processors/aggregators/user_response.py
@@ -74,6 +74,8 @@ class ResponseAggregator(FrameProcessor):
     #    S I T E -> X
     #    S I E T -> X
     #  S I E I T -> X
+    #      S E T -> X
+    #    S E I T -> X
     #
     # The following case would not be supported:
     #
@@ -99,7 +101,7 @@ class ResponseAggregator(FrameProcessor):
             # We might have received the end frame but we might still be
             # aggregating (i.e. we have seen interim results but not the final
             # text).
-            self._aggregating = self._seen_interim_results
+            self._aggregating = self._seen_interim_results or len(self._aggregation) == 0
 
             # Send the aggregation if we are not aggregating anymore (i.e. no
             # more interim results received).
