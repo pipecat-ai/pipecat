2b84abae67695babaf3e68a1a5002f4e390f7921
TomTom101 <thobra@gmail.com>
2024-07-06 11:11:40 +0200

    chore: Combined combinable lookaheads

--- src/pipecat/services/ai_services.py
+++ src/pipecat/services/ai_services.py
@@ -35,9 +35,7 @@ ENDOFSENTENCE_PATTERN_STR = r"""
     (?<![A-Z])       # Negative lookbehind: not preceded by an uppercase letter (e.g., "U.S.A.")
     (?<!\d)          # Negative lookbehind: not preceded by a digit (e.g., "1. Let's start")
     (?<!\d\s[ap])    # Negative lookbehind: not preceded by time (e.g., "3:00 a.m.")
-    (?<!Mr)          # Negative lookbehind: not preceded by "Mr"
-    (?<!Ms)          # Negative lookbehind: not preceded by "Ms"
-    (?<!Dr)          # Negative lookbehind: not preceded by "Dr"
+    (?<!Mr|Ms|Dr)    # Negative lookbehind: not preceded by Mr, Ms, Dr (combined bc. length is the same)
     (?<!Mrs)         # Negative lookbehind: not preceded by "Mrs"
     (?<!Prof)        # Negative lookbehind: not preceded by "Prof"
     [\.\?\!:]        # Match a period, question mark, exclamation point, or colon
