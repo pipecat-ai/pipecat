cfeaeaeb79f66720113d828547d2cf5af3d8e6c6
Chad Bailey <chadbailey@gmail.com>
2024-08-09 14:32:50 -0500

    added a single function call handler

--- src/pipecat/services/ai_services.py
+++ src/pipecat/services/ai_services.py
@@ -115,28 +115,38 @@ class LLMService(AIService):
         self._start_callbacks = {}
 
     # TODO-CB: callback function type
-    def register_function(self, function_name: str, callback, start_callback=None):
+    def register_function(self, function_name: str | None, callback, start_callback=None):
+        if not function_name: # then use this callback for all functions
+            function_name = "*" 
         self._callbacks[function_name] = callback
         if start_callback:
             self._start_callbacks[function_name] = start_callback
+        
 
-    def unregister_function(self, function_name: str):
+    def unregister_function(self, function_name: str | None):
+        if not function_name: # then use this callback for all functions
+            function_name = "*"
         del self._callbacks[function_name]
         if self._start_callbacks[function_name]:
             del self._start_callbacks[function_name]
 
     def has_function(self, function_name: str):
+        if "*" in self._callbacks.keys():
+            return True
         return function_name in self._callbacks.keys()
 
     async def call_function(self, function_name: str, args):
         if function_name in self._callbacks.keys():
             return await self._callbacks[function_name](self, args)
+        elif "*" in self._callbacks.keys():
+            return await self._callbacks["*"](self, function_name, args)
         return None
 
     async def call_start_function(self, function_name: str):
         if function_name in self._start_callbacks.keys():
             await self._start_callbacks[function_name](self)
-
+        elif "*" in self._callbacks.keys():
+            return await self._start_callbacks["*"](self, function_name)
 
 class TTSService(AIService):
     def __init__(
