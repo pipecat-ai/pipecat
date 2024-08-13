29ca1b7855a29149fa195bbaa899177816b1c3e4
Kwindla Hultman Kramer <kwindla@gmail.com>
2024-08-13 11:01:24 -0700

    Anthropic tool use core Pipecat pieces refactored (#369)
    * processors(rtvi): rtvi 0.1 message protocol
    
    * added a single function call handler
    
    * wip - function calling
    
    * fixup
    
    * fixup
    
    * fixup
    
    * processors(rtvi): no need for configure_on_start()
    
    * processors(rtvi): add new option values if they haven't been set yet
    
    * Add the model name to the LLM usage metrics
    
    * wip - anthropic tool calling
    
    * still wip - anthropic tool use and vision
    
    * anthropic tools and vision working
    
    * anthropic tool calling and vision
    
    * Cartesia error handling
    
    * Anthropic tool use core Pipecat pieces refactored as per plan
    
    * aleix has good ideas
    
    * Usage metrics for Anthropic LLMs
    
    * fix function call result state not getting cleared bug
    
    * Pass **kwargs through from AnthropicLLMService constructor
    
    * about to tinker with anthropic
    
    * added openai function calling
    
    * openai function calling
    
    * fixup
    
    ---------
    
    Co-authored-by: Aleix Conchillo Flaqué <aleix@daily.co>
    Co-authored-by: Chad Bailey <chadbailey@gmail.com>
    Co-authored-by: mattie ruth backman <mattieruth@gmail.com>
    Co-authored-by: chadbailey59 <chadbailey59@users.noreply.github.com>

--- src/pipecat/frames/frames.py
+++ src/pipecat/frames/frames.py
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: BSD 2-Clause License
 #
 
-from typing import Any, List, Mapping, Tuple
+from typing import Any, List, Mapping, Tuple, Optional
 
 from dataclasses import dataclass, field
 
@@ -175,6 +175,15 @@ class LLMMessagesUpdateFrame(DataFrame):
 
     """
     messages: List[dict]
+
+
+@dataclass
+class LLMSetToolsFrame(DataFrame):
+    """A frame containing a list of tools for an LLM to use for function calling.
+    The specific format depends on the LLM being used, but it should typically
+    contain JSON Schema objects.
+    """
+    tools: List[dict]
 
 
 @dataclass
@@ -389,6 +398,7 @@ class TTSStoppedFrame(ControlFrame):
 class UserImageRequestFrame(ControlFrame):
     """A frame user to request an image from the given user."""
     user_id: str
+    context: Optional[any]
 
     def __str__(self):
         return f"{self.name}, user: {self.user_id}"
@@ -406,3 +416,22 @@ class TTSVoiceUpdateFrame(ControlFrame):
     """A control frame containing a request to update to a new TTS voice.
     """
     voice: str
+
+
+@dataclass
+class FunctionCallInProgressFrame(SystemFrame):
+    """A frame signaling that a function call is in progress.
+    """
+    function_name: str
+    tool_call_id: str
+    arguments: str
+
+
+@dataclass
+class FunctionCallResultFrame(DataFrame):
+    """A frame containing the result of an LLM function (tool) call.
+    """
+    function_name: str
+    tool_call_id: str
+    arguments: str
+    result: any
--- src/pipecat/services/ai_services.py
+++ src/pipecat/services/ai_services.py
@@ -25,11 +25,14 @@ from pipecat.frames.frames import (
     TTSVoiceUpdateFrame,
     TextFrame,
     VisionImageRawFrame,
+    FunctionCallResultFrame
 )
 from pipecat.processors.async_frame_processor import AsyncFrameProcessor
 from pipecat.processors.frame_processor import FrameDirection, FrameProcessor
 from pipecat.utils.audio import calculate_audio_volume
 from pipecat.utils.utils import exp_smoothing
+from pipecat.processors.aggregators.openai_llm_context import OpenAILLMContext
+
 import re
 
 
@@ -115,27 +118,51 @@ class LLMService(AIService):
         self._start_callbacks = {}
 
     # TODO-CB: callback function type
-    def register_function(self, function_name: str, callback, start_callback=None):
+    def register_function(self, function_name: str | None, callback, start_callback=None):
+        # Registering a function with the function_name set to None will run that callback
+        # for all functions
         self._callbacks[function_name] = callback
+        # QUESTION FOR CB: maybe this isn't needed anymore?
         if start_callback:
             self._start_callbacks[function_name] = start_callback
 
-    def unregister_function(self, function_name: str):
+    def unregister_function(self, function_name: str | None):
         del self._callbacks[function_name]
         if self._start_callbacks[function_name]:
             del self._start_callbacks[function_name]
 
     def has_function(self, function_name: str):
+        if None in self._callbacks.keys():
+            return True
         return function_name in self._callbacks.keys()
 
-    async def call_function(self, function_name: str, args):
+    async def call_function(
+            self,
+            *,
+            context: OpenAILLMContext,
+            tool_call_id: str,
+            function_name: str,
+            arguments: str) -> None:
+        f = None
         if function_name in self._callbacks.keys():
-            return await self._callbacks[function_name](self, args)
-        return None
+            f = self._callbacks[function_name]
+        elif None in self._callbacks.keys():
+            f = self._callbacks[None]
+        else:
+            return None
+        await context.call_function(
+            f,
+            function_name=function_name,
+            tool_call_id=tool_call_id,
+            arguments=arguments,
+            llm=self)
 
+    # QUESTION FOR CB: maybe this isn't needed anymore?
     async def call_start_function(self, function_name: str):
         if function_name in self._start_callbacks.keys():
             await self._start_callbacks[function_name](self)
+        elif None in self._start_callbacks.keys():
+            return await self._start_callbacks[None](function_name)
 
 
 class TTSService(AIService):
@@ -151,12 +178,12 @@ class TTSService(AIService):
         self._push_text_frames: bool = push_text_frames
         self._current_sentence: str = ""
 
-    @abstractmethod
+    @ abstractmethod
     async def set_voice(self, voice: str):
         pass
 
     # Converts the text to audio.
-    @abstractmethod
+    @ abstractmethod
     async def run_tts(self, text: str) -> AsyncGenerator[Frame, None]:
         pass
 
@@ -242,7 +269,7 @@ class STTService(AIService):
         self._smoothing_factor = 0.2
         self._prev_volume = 0
 
-    @abstractmethod
+    @ abstractmethod
     async def run_stt(self, audio: bytes) -> AsyncGenerator[Frame, None]:
         """Returns transcript as a string"""
         pass
@@ -308,7 +335,7 @@ class ImageGenService(AIService):
         super().__init__(**kwargs)
 
     # Renders the image. Returns an Image object.
-    @abstractmethod
+    @ abstractmethod
     async def run_image_gen(self, prompt: str) -> AsyncGenerator[Frame, None]:
         pass
 
@@ -331,7 +358,7 @@ class VisionService(AIService):
         super().__init__(**kwargs)
         self._describe_text = None
 
-    @abstractmethod
+    @ abstractmethod
     async def run_vision(self, frame: VisionImageRawFrame) -> AsyncGenerator[Frame, None]:
         pass
 
