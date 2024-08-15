959580a7082eeff09c4ec24b2458cc558c599d26
Aleix Conchillo Flaqué <aleix@daily.co>
2024-08-14 20:39:24 -0700

    processors(logger): fix linting

--- src/pipecat/processors/logger.py
+++ src/pipecat/processors/logger.py
@@ -12,12 +12,18 @@ logger = logger.opt(ansi=True)
 
 
 class FrameLogger(FrameProcessor):
-    def __init__(self, prefix="Frame", color: Optional[str] = None, ignored_frame_types: Optional[list] = [BotSpeakingFrame, AudioRawFrame, TransportMessageFrame]):
+    def __init__(
+        self,
+        prefix="Frame",
+        color: Optional[str] = None,
+        ignored_frame_types: Optional[list] = [
+            BotSpeakingFrame,
+            AudioRawFrame,
+            TransportMessageFrame]):
         super().__init__()
         self._prefix = prefix
         self._color = color
         self._ignored_frame_types = tuple(ignored_frame_types) if ignored_frame_types else None
-        
 
     async def process_frame(self, frame: Frame, direction: FrameDirection):
         if self._ignored_frame_types and not isinstance(frame, self._ignored_frame_types):
