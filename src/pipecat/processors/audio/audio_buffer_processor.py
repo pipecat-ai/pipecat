2e02ab740d13d23964e1b7bc2c2735923f6307fa
Adrian Cowham <adrian.cowham@gmail.com>
2024-09-15 20:59:17 -0700

    PR feedback

--- src/pipecat/processors/audio/audio_buffer_processor.py
+++ src/pipecat/processors/audio/audio_buffer_processor.py
@@ -1,6 +1,7 @@
 from pipecat.frames.frames import (AudioRawFrame, BotStartedSpeakingFrame,
                                    BotStoppedSpeakingFrame, Frame,
-                                   UserAudioFrame, UserStoppedSpeakingFrame)
+                                   UserAudioFrame, UserStartedSpeakingFrame,
+                                   UserStoppedSpeakingFrame)
 from pipecat.processors.frame_processor import FrameDirection, FrameProcessor
 
 
@@ -21,54 +22,61 @@ class AudioBufferProcessor(FrameProcessor):
         populated when the first audio frame is processed.
         """
         super().__init__()
-        self.audio_buffer = bytearray()
-        self.num_channels = None
-        self.sample_rate = None
-        self.assistant_audio = False
-        self.user_audio = False
+        self._audio_buffer = bytearray()
+        self._num_channels = None
+        self._sample_rate = None
+        self._assistant_audio = False
+        self._user_audio = False
+        print(f"ctor::AudioBufferProcessor object memory address: {id(self)}")
 
-    def has_audio(self):
+    def _has_audio(self):
         return (
-            self.audio_buffer and
-            len(self.audio_buffer) > 0 and
-            self.num_channels and
-            self.sample_rate
+            self._audio_buffer is not None and
+            len(self._audio_buffer) > 0 and
+            self._num_channels is not None and
+            self._sample_rate is not None
         )
+
+    def _reset_audio_buffer(self):
+        self._audio_buffer = bytearray()
 
     async def process_frame(self, frame: Frame, direction: FrameDirection):
         await super().process_frame(frame, direction)
-        if isinstance(frame, AudioRawFrame) or isinstance(frame, UserAudioFrame):
-            if self.num_channels is None:
-                self.num_channels = frame.num_channels
-            if self.sample_rate is None:
-                self.sample_rate = frame.sample_rate
+        if isinstance(frame, AudioRawFrame):
+            if self._num_channels is None:
+                self._num_channels = frame.num_channels
+            if self._sample_rate is None:
+                self._sample_rate = frame.sample_rate
 
-        elif isinstance(frame, UserStoppedSpeakingFrame):
-            self.user_audio = False
+        if isinstance(frame, UserStoppedSpeakingFrame):
+            self._user_audio = False
 
         if isinstance(frame, BotStartedSpeakingFrame):
-            self.assistant_audio = True
-            self.user_audio = False  # do not capture user audio if assistant is speaking
+            self._assistant_audio = True
+            self._user_audio = False  # do not capture user audio if assistant is speaking
+
         if isinstance(frame, BotStoppedSpeakingFrame):
-            self.assistant_audio = False
+            self._assistant_audio = False
             # Capture user audio if assistant is not speaking, even if it's silence, the point
             # here is to capture so that the conversation is as close to reality as possible.
             # This is important for evaluation and metrics capture.
-            self.user_audio = True
+            self._user_audio = True
 
         # only include audio from the user if the user is speaking, this is because audio from the user's
         # mic is always coming in. if we include all the user's audio there will be a long latency before
         # the user starts speaking because all of the user's silence during the assistant's speech will have been
         # added to the buffer.
-        if isinstance(frame, UserAudioFrame) and self.user_audio:
-            self.audio_buffer.extend(frame.audio)
+        #
+        # and include all audio from the assistant
+        if isinstance(frame, UserAudioFrame) and self._user_audio:
+            self._audio_buffer.extend(frame.audio)
 
         # include all audio from the assistant
         if (
             isinstance(frame, AudioRawFrame)
             and not isinstance(frame, UserAudioFrame)
         ):
-            self.audio_buffer.extend(frame.audio)
+            self._audio_buffer.extend(frame.audio)
 
         # do not push the user's audio frame, doing so will result in echo
         if not isinstance(frame, UserAudioFrame):
