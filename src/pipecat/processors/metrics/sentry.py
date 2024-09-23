f7f4c9517860345086e9bba7e1f4416c18ecef92
cs-metadexlabs <salvan.cyril.es@gmail.com>
2024-09-23 11:56:16 +0200

    Format with autopep8 & adjust logger level

--- src/pipecat/processors/metrics/sentry.py
+++ src/pipecat/processors/metrics/sentry.py
@@ -5,12 +5,15 @@ try:
     import sentry_sdk
     sentry_available = sentry_sdk.is_initialized()
     if not sentry_available:
-        logger.debug("Sentry SDK not initialized. Sentry features will be disabled.")
+        logger.warning(
+            "Sentry SDK not initialized. Sentry features will be disabled.")
 except ImportError:
     sentry_available = False
-    logger.debug("Sentry SDK not installed. Sentry features will be disabled.")
+    logger.warning(
+        "Sentry SDK not installed. Sentry features will be disabled.")
 
 from pipecat.processors.metrics.frame_processor_metrics import FrameProcessorMetrics
+
 
 class SentryMetrics(FrameProcessorMetrics):
     def __init__(self):
@@ -23,11 +26,12 @@ class SentryMetrics(FrameProcessorMetrics):
             self._start_ttfb_time = time.time()
             if sentry_available:
                 self._ttfb_metrics_span = sentry_sdk.start_span(
-                    op="ttfb", 
+                    op="ttfb",
                     description=f"TTFB for {self._processor_name()}",
                     start_timestamp=self._start_ttfb_time
                 )
-                logger.debug(f"Sentry Span ID: {self._ttfb_metrics_span.span_id} Description: {self._ttfb_metrics_span.description} started.")
+                logger.debug(f"Sentry Span ID: {self._ttfb_metrics_span.span_id} Description: {
+                             self._ttfb_metrics_span.description} started.")
             self._should_report_ttfb = not report_only_initial_ttfb
 
     async def stop_ttfb_metrics(self):
@@ -39,12 +43,12 @@ class SentryMetrics(FrameProcessorMetrics):
         self._start_processing_time = time.time()
         if sentry_available:
             self._processing_metrics_span = sentry_sdk.start_span(
-                op="processing", 
+                op="processing",
                 description=f"Processing for {self._processor_name()}",
                 start_timestamp=self._start_processing_time
             )
-            logger.debug(f"Sentry Span ID: {self._processing_metrics_span.span_id} Description: {self._processing_metrics_span.description} started.")
-
+            logger.debug(f"Sentry Span ID: {self._processing_metrics_span.span_id} Description: {
+                         self._processing_metrics_span.description} started.")
 
     async def stop_processing_metrics(self):
         stop_time = time.time()
