c489db3b2f39c65cf4bc1ceec8e325188ca8faa2
Kwindla Hultman Kramer <kwindla@gmail.com>
2024-10-04 13:37:17 -0700

    definitely broke something in the pipeline

--- examples/foundational/19-openai-realtime-beta.py
+++ examples/foundational/19-openai-realtime-beta.py
@@ -132,7 +132,7 @@ Remember, your responses should be short. Just one or two sentences, usually.
                 allow_interruptions=True,
                 enable_metrics=True,
                 enable_usage_metrics=True,
-                report_only_initial_ttfb=True,
+                # report_only_initial_ttfb=True,
             ),
         )
 
