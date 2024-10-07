d892f562e3c43d672de6d21777897c5249cd180a
Allenmylath <155647926+Allenmylath@users.noreply.github.com>
2024-10-07 21:34:49 +0530

    Create ola_krutrim.py
    new tts service using ola_krutrim tts.This cheap tts with lot of free credits may help hobbyists and founders.

---
+++ src/pipecat/services/ola_krutrim.py
@@ -0,0 +1,104 @@
+import base64
+import json
+from typing import AsyncGenerator
+
+import aiohttp
+from loguru import logger
+from pydantic import BaseModel
+
+from pipecat.frames.frames import (
+    CancelFrame,
+    EndFrame,
+    Frame,
+    TTSAudioRawFrame,
+    TTSStartedFrame,
+    TTSStoppedFrame,
+)
+from pipecat.services.ai_services import TTSService
+from pipecat.transcriptions.language import Language
+
+
+def language_to_ola_krutrim_language(language: Language) -> str | None:
+    # Implement language mapping when Ola Krutrim supports different languages
+    # For now, we'll return None 
+    return None
+
+
+class OlaKrutrimTTSService(TTSService):
+    class InputParams(BaseModel):
+        model_name: str = "fastpitch_tts"
+        base_url: str = "https://cloud.olakrutrim.com/v1/audio/generations/tts"
+
+    def __init__(
+        self,
+        *,
+        api_key: str,
+        params: InputParams = InputParams(),
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+        self._api_key = api_key
+        self._model_name = params.model_name
+        self._base_url = params.base_url
+        self.set_model_name(params.model_name)
+
+    async def set_model(self, model: str):
+        logger.debug(f"Switching TTS model to: [{model}]")
+        self._model_name = model
+        await super().set_model(model)
+
+    async def set_language(self, language: Language):
+        # Implement when Krutrim supports language selection
+        pass
+
+    async def run_tts(self, text: str) -> AsyncGenerator[Frame, None]:
+        logger.debug(f"Generating TTS: [{text}]")
+        await self.push_frame(TTSStartedFrame())
+        await self.start_ttfb_metrics()
+
+        try:
+            payload = {
+                "modelName": self._model_name,
+                "textToSpeak": text,
+            }
+            headers = {
+                "content-type": "application/json",
+                "Authorization": f"Bearer {self._api_key}",
+            }
+            url = f"{self._base_url}/{self._model_name}"
+
+            async with aiohttp.ClientSession() as session:
+                async with session.post(url, headers=headers, json=payload) as resp:
+                    if resp.status == 200:
+                        await self.stop_ttfb_metrics()
+                        resp_json = await resp.json()
+                        output = resp_json["output"]
+                        sr = resp_json["sampling_rate"]
+
+                        op_audio = base64.b64decode(output)
+                        op_audio = json.loads(op_audio)
+
+                        frame = TTSAudioRawFrame(
+                            audio=op_audio,
+                            sample_rate=sr,
+                            num_channels=1,
+                        )
+                        yield frame
+                    else:
+                        logger.error(
+                            f"Failed to get a response. Status code: {resp.status}"
+                        )
+                        logger.error(f"Response content: {await resp.text()}")
+                        raise Exception("Failed to generate TTS")
+
+        except Exception as e:
+            logger.error(f"{self} exception: {e}")
+
+        await self.start_tts_usage_metrics(text)
+        await self.push_frame(TTSStoppedFrame())
+
+    async def stop(self, frame: EndFrame):
+        await super().stop(frame)
+
+    async def cancel(self, frame: CancelFrame):
+        await super().cancel(frame)
