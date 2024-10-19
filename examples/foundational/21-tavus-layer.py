289d17598ab1408a4e938467feb6b394c550ec17
roey <159067767+roey-tavus@users.noreply.github.com>
2024-10-18 21:53:26 -0400

    initial work

---
+++ examples/foundational/21-tavus-layer.py
@@ -0,0 +1,120 @@
+#
+# Copyright (c) 2024, Daily
+#
+# SPDX-License-Identifier: BSD 2-Clause License
+#
+
+import asyncio
+import aiohttp
+import os
+import sys
+
+from pipecat.frames.frames import LLMMessagesFrame
+from pipecat.pipeline.pipeline import Pipeline
+from pipecat.pipeline.runner import PipelineRunner
+from pipecat.pipeline.task import PipelineParams, PipelineTask
+from pipecat.processors.aggregators.llm_response import (
+    LLMAssistantResponseAggregator,
+    LLMUserResponseAggregator,
+)
+from pipecat.services.cartesia import CartesiaTTSService
+from pipecat.services.openai import OpenAILLMService
+from pipecat.services.deepgram import DeepgramSTTService
+from pipecat.services.tavus import TavusVideoService
+from pipecat.transports.services.daily import DailyParams, DailyTransport
+from pipecat.vad.silero import SileroVADAnalyzer
+
+from loguru import logger
+
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
+
+logger.remove(0)
+logger.add(sys.stderr, level="DEBUG")
+
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        # (room_url, token) = await configure(session)
+
+        room_url, conversation_id = TavusVideoService._initiate_conversation(
+            api_key=os.getenv("TAVUS_API_KEY"),
+            replica_id=os.getenv("TAVUS_REPLICA_ID"),
+            properties={}
+        )
+
+        transport = DailyTransport(
+            room_url=room_url,
+            token=None,
+            bot_name="Respond bot",
+            params=DailyParams(
+                audio_out_enabled=True,
+                transcription_enabled=True,
+                vad_enabled=True,
+                vad_analyzer=SileroVADAnalyzer(),
+            ),
+        )
+
+        stt = DeepgramSTTService(api_key=os.getenv("DEEPGRAM_API_KEY"))
+
+        tts = CartesiaTTSService(
+            api_key=os.getenv("CARTESIA_API_KEY"),
+            voice_id="79a125e8-cd45-4c13-8a67-188112f4dd22",  # British Lady
+        )
+
+        llm = OpenAILLMService(model="gpt-4o-mini")
+
+        messages = [
+            {
+                "role": "system",
+                "content": "You are a helpful LLM in a WebRTC call. Your goal is to demonstrate your capabilities in a succinct way. Your output will be converted to audio so don't include special characters in your answers. Respond to what the user said in a creative and helpful way.",
+            },
+        ]
+
+        tma_in = LLMUserResponseAggregator(messages)
+        tma_out = LLMAssistantResponseAggregator(messages)
+
+        tavus = TavusVideoService(
+            conversation_id=conversation_id,
+            client=transport._client,
+            params=transport._params
+        )
+
+        pipeline = Pipeline(
+            [
+                transport.input(),  # Transport user input
+                stt,  # STT
+                tma_in,  # User responses
+                llm,  # LLM
+                tts,  # TTS
+                tavus, # Tavus output layer
+                # transport.output(),  # Transport bot output
+                tma_out,  # Assistant spoken responses
+            ]
+        )
+
+        task = PipelineTask(
+            pipeline,
+            PipelineParams(
+                allow_interruptions=True,
+                enable_metrics=True,
+                enable_usage_metrics=True,
+                report_only_initial_ttfb=True,
+            ),
+        )
+
+        @transport.event_handler("on_first_participant_joined")
+        async def on_first_participant_joined(transport, participant):
+            transport.capture_participant_transcription(participant["id"])
+            # Kick off the conversation.
+            messages.append({"role": "system", "content": "Please introduce yourself to the user."})
+            await task.queue_frames([LLMMessagesFrame(messages)])
+
+        runner = PipelineRunner()
+
+        await runner.run(task)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ src/pipecat/services/tavus.py
@@ -0,0 +1,107 @@
+#
+# Copyright (c) 2024, Daily
+#
+# SPDX-License-Identifier: BSD 2-Clause License
+#
+
+"""This module implements Whisper transcription with a locally-downloaded model."""
+
+import asyncio
+from typing import Any
+import base64
+import requests
+
+from enum import Enum
+from typing import AsyncGenerator
+
+import numpy as np
+
+from pipecat.frames.frames import ErrorFrame, Frame, TranscriptionFrame, TTSAudioRawFrame, TransportMessageUrgentFrame
+from pipecat.processors.frame_processor import FrameDirection, FrameProcessor
+from pipecat.services.ai_services import AIService
+from pipecat.transports.base_output import BaseOutputTransport
+from pipecat.transports.services.daily import DailyTransportClient, DailyParams
+from pipecat.utils.time import time_now_iso8601
+
+from loguru import logger
+
+class TavusVideoService(BaseOutputTransport):
+    """Class to send base64 encoded audio to Tavus"""
+
+    def __init__(
+        self,
+        *,
+        conversation_id: str,
+        client: DailyTransportClient,
+        params: DailyParams,
+        **kwargs,
+    ) -> None:
+        super().__init__(params, **kwargs)
+        self._client = client
+        self._conversation_id = conversation_id
+
+
+    def can_generate_metrics(self) -> bool:
+        return True
+
+    @classmethod
+    def _initiate_conversation(
+        cls,
+        api_key: str,
+        replica_id: str,
+        persona_id: str,
+        properties: dict[str, Any] = {}
+    ) -> tuple[str, str]:
+        url = "https://tavusapi.com/v2/conversations"
+        headers = {
+            "Content-Type": "application/json",
+            "x-api-key": api_key
+        }
+        payload = {
+            "replica_id": replica_id,
+            "persona_id": persona_id,
+            "properties": properties
+        }
+
+        response = requests.post(url, headers=headers, json=payload)
+        response.raise_for_status()
+
+        response_json = response.json()
+        logger.debug(f"TavusVideoService joined {response_json['conversation_url']}")
+        return response_json["conversation_url"], response_json["conversation_id"]
+
+    async def encode_audio_and_send(self, audio: bytes) -> None:
+        """Encodes audio to base64 and sends it to Tavus"""
+        await self.start_processing_metrics()
+        await self.start_ttfb_metrics()
+
+        # Divide by 32768 because we have signed 16-bit data.
+        audio_float = np.frombuffer(audio, dtype=np.int16).astype(np.float32) / 32768.0
+        audio_base64 = base64.b64encode(audio_float).decode("utf-8")
+
+        await self.send_message(audio_base64)
+        await self.stop_ttfb_metrics()
+        await self.stop_processing_metrics()
+
+    async def process_frame(self, frame: Frame, direction: FrameDirection):
+        await super().process_frame(frame, direction)
+        if isinstance(frame, TTSAudioRawFrame):
+            await self.encode_audio_and_send(frame.audio)
+        else:
+            await self.push_frame(frame, direction)
+
+    async def send_message(self, audio_base64: str) -> None:
+        message = TransportMessageUrgentFrame(
+            message={
+                "message": {
+                "message_type": "conversation",
+                "event_type": "conversation.echo",
+                "conversation_id": self._conversation_id,
+                "properties": {
+                    "type": "audio",
+                    "audio": audio_base64
+                }
+                }
+            }
+        )
+        await self._client.send_message(message)
