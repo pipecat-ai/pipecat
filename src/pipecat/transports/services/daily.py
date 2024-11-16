cbb0e8c353d09502c16a1db9aa404ac288653940
Nikita Gamolsky <nikita.gamolsky@gmail.com>
2024-11-16 11:14:30 -0800

    add screenVideo subscription for daily transport service

--- src/pipecat/transports/services/daily.py
+++ src/pipecat/transports/services/daily.py
@@ -499,9 +499,11 @@ class DailyTransportClient(EventHandler):
         video_source: str = "camera",
         color_format: str = "RGB",
     ):
-        # Only enable camera subscription on this participant
+        # Try to enable camera and screen subscription on this participant
         await self.update_subscriptions(
-            participant_settings={participant_id: {"media": {"camera": "subscribed"}}}
+            participant_settings={
+                participant_id: {"media": {"camera": "subscribed", "screenVideo": "subscribed"}}
+            }
         )
 
         self._video_renderers[participant_id] = callback
