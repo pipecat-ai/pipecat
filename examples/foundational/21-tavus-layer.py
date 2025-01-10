cb22de0d1392499b040373113c95565d3b77bdd6
Mark Backman <mark@daily.co>
2025-01-10 08:01:00 -0500

    Update the Tavus example and comment about using the PERSONA_ID

--- examples/foundational/21-tavus-layer.py
+++ examples/foundational/21-tavus-layer.py
@@ -39,7 +39,6 @@ async def main():
         tavus = TavusVideoService(
             api_key=os.getenv("TAVUS_API_KEY"),
             replica_id=os.getenv("TAVUS_REPLICA_ID"),
-            persona_id=os.getenv("TAVUS_PERSONA_ID", "pipecat0"),
             session=session,
         )
 
