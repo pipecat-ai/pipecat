c2fef9584bdacbe5732c875ee0e71446ebc7d852
James Hush <james@daily.co>
2025-01-23 15:54:53 +0800

    Add call transfer to bot_daily

--- examples/foundational/32-call-transfer.py
+++ examples/foundational/32-call-transfer.py
@@ -1,51 +1,50 @@
+#
 # Copyright (c) 2024–2025, Daily
 #
 # SPDX-License-Identifier: BSD 2-Clause License
 #
 
-import argparse
+
 import asyncio
 import os
 import sys
 
-from dotenv import load_dotenv
 from loguru import logger
 from openai.types.chat import ChatCompletionToolParam
 
 from pipecat.audio.vad.silero import SileroVADAnalyzer
-from pipecat.frames.frames import EndFrame
+from pipecat.frames.frames import EndFrame, LLMMessagesFrame
 from pipecat.pipeline.pipeline import Pipeline
 from pipecat.pipeline.runner import PipelineRunner
 from pipecat.pipeline.task import PipelineParams, PipelineTask
 from pipecat.processors.aggregators.openai_llm_context import OpenAILLMContext
 from pipecat.services.ai_services import LLMService
+from pipecat.services.cartesia import CartesiaTTSService
 from pipecat.services.deepgram import DeepgramSTTService
-from pipecat.services.elevenlabs import ElevenLabsTTSService
 from pipecat.services.openai import OpenAILLMService
 from pipecat.transports.services.daily import DailyDialinSettings, DailyParams, DailyTransport
-
-load_dotenv(override=True)
 
 logger.remove(0)
 logger.add(sys.stderr, level="DEBUG")
 
-daily_api_key = os.getenv("DAILY_API_KEY", "")
-daily_api_url = os.getenv("DAILY_API_URL", "https://api.daily.co/v1")
 
+async def main(room_url: str, token: str, call_id: str, call_domain: str):
+    """Main entrypoint for the voice bot process.
 
-async def main(room_url: str, token: str, callId: str, callDomain: str):
-    # diallin_settings are only needed if Daily's SIP URI is used
-    # If you are handling this via Twilio, Telnyx, set this to None
-    # and handle call-forwarding when on_dialin_ready fires.
-    diallin_settings = DailyDialinSettings(call_id=callId, call_domain=callDomain)
+    :param room_url: The Daily.co room URL
+    :param token: The Daily.co token
+    :param callId: The call ID from Daily.co
+    :param callDomain: The domain associated with the call
+    """
+    diallin_settings = DailyDialinSettings(call_id=call_id, call_domain=call_domain)
 
     transport = DailyTransport(
         room_url,
         token,
         "Chatbot",
         DailyParams(
-            api_url=daily_api_url,
-            api_key=daily_api_key,
+            api_url="https://api.daily.co/v1/",
+            api_key=os.getenv("DAILY_API_KEY", ""),
             dialin_settings=diallin_settings,
             audio_in_enabled=True,
             audio_out_enabled=True,
@@ -56,24 +55,20 @@ async def main(room_url: str, token: str, callId: str, callDomain: str):
         ),
     )
 
-    tts = ElevenLabsTTSService(
-        api_key=os.getenv("ELEVENLABS_API_KEY", ""),
-        voice_id=os.getenv("ELEVENLABS_VOICE_ID", ""),
+    cartesia_params = CartesiaTTSService.InputParams(
+        speed=-0.1,
+        emotion=["positivity:high", "curiosity"],
+        language="en",
+    )
+
+    tts = CartesiaTTSService(
+        api_key=os.getenv("CARTESIA_API_KEY", ""),
+        # Use Helpful Woman voice by default
+        voice_id="156fb8d2-335b-4950-9cb3-a2d33befec77",
+        params=cartesia_params,
     )
 
     llm = OpenAILLMService(api_key=os.getenv("OPENAI_API_KEY"), model="gpt-4o")
-
-    content = f"""
-You are a delivery service customer support specialist supporting customers with their orders. 
-Begin with: "Hello, this is Hailey from customer support. What can I help you with today?"
-    """
-
-    messages = [
-        {
-            "role": "system",
-            "content": content,
-        },
-    ]
 
     tools = [
         ChatCompletionToolParam(
@@ -86,7 +81,7 @@ Begin with: "Hello, this is Hailey from customer support. What can I help you wi
                     "properties": {
                         "call_id": {
                             "type": "string",
-                            "description": "This is always {callId}.",
+                            "description": "This is always {call_id}.",
                         },
                         "summary": {
                             "type": "string",
@@ -100,12 +95,26 @@ Provide a concise summary in 3-5 sentences. Highlight any  important details or 
         )
     ]
 
+    content = f"""
+You are a delivery service customer support specialist supporting customers with their orders. 
+Begin with: "Hello, this is Hailey from customer support. What can I help you with today?"
+    """
+
+    messages = [
+        {
+            "role": "system",
+            "content": content,
+        },
+    ]
+
     context = OpenAILLMContext(messages, tools)
     context_aggregator = llm.create_context_aggregator(context)
+    stt = DeepgramSTTService(api_key=os.getenv("DEEPGRAM_API_KEY"))
 
     pipeline = Pipeline(
         [
             transport.input(),
+            stt,  # STT
             context_aggregator.user(),
             llm,
             tts,
@@ -117,12 +126,14 @@ Provide a concise summary in 3-5 sentences. Highlight any  important details or 
     task = PipelineTask(pipeline, PipelineParams(allow_interruptions=True))
 
     @transport.event_handler("on_first_participant_joined")
-    async def on_first_participant_joined(transport, participant):
-        await transport.capture_participant_transcription(participant["id"])
-        await task.queue_frames([context_aggregator.user().get_context_frame()])
+    async def on_first_participant_joined(_, participant):
+        logger.info(f"on_first_participant_joined: {participant}")
+        # await transport.capture_participant_transcription(participant["id"]) Might not need this
+        await task.queue_frames([LLMMessagesFrame(messages)])
 
     @transport.event_handler("on_participant_left")
-    async def on_participant_left(transport, participant, reason):
+    async def on_participant_left(_, participant, reason):
+        logger.info(f"on_participant_left: {participant} {reason}")
         await task.queue_frame(EndFrame())
 
     @transport.event_handler("on_dialin_ready")
@@ -169,11 +180,4 @@ Provide a concise summary in 3-5 sentences. Highlight any  important details or 
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Pipecat Simple ChatBot")
-    parser.add_argument("-u", type=str, help="Room URL")
-    parser.add_argument("-t", type=str, help="Token")
-    parser.add_argument("-i", type=str, help="Call ID")
-    parser.add_argument("-d", type=str, help="Call Domain")
-    config = parser.parse_args()
-
-    asyncio.run(main(config.u, config.t, config.i, config.d))
+    asyncio.run(main())
