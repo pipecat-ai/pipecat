179ddbea7d4bfaf2a3ab23f0492e4ae0ffc40296
chadbailey59 <chadbailey59@users.noreply.github.com>
2025-01-27 12:21:30 -0600

    Add dialout to the Daily phone example (#998)
    * added dialout to daily phone example
    
    * cleanup
    
    * cleanup
    
    * pre-commit hook
    
    * Fix typo
    
    * More explicit README instructions
    
    ---------
    
    Co-authored-by: Mark Backman <mark@daily.co>

--- examples/phone-chatbot/bot_daily.py
+++ examples/phone-chatbot/bot_daily.py
@@ -25,12 +25,11 @@ daily_api_key = os.getenv("DAILY_API_KEY", "")
 daily_api_url = os.getenv("DAILY_API_URL", "https://api.daily.co/v1")
 
 
-async def main(room_url: str, token: str, callId: str, callDomain: str):
-    # diallin_settings are only needed if Daily's SIP URI is used
+async def main(room_url: str, token: str, callId: str, callDomain: str, dialout_number: str | None):
+    # dialin_settings are only needed if Daily's SIP URI is used
     # If you are handling this via Twilio, Telnyx, set this to None
     # and handle call-forwarding when on_dialin_ready fires.
-    diallin_settings = DailyDialinSettings(call_id=callId, call_domain=callDomain)
-
+    dialin_settings = DailyDialinSettings(call_id=callId, call_domain=callDomain)
     transport = DailyTransport(
         room_url,
         token,
@@ -38,7 +37,7 @@ async def main(room_url: str, token: str, callId: str, callDomain: str):
         DailyParams(
             api_url=daily_api_url,
             api_key=daily_api_key,
-            dialin_settings=diallin_settings,
+            dialin_settings=dialin_settings,
             audio_in_enabled=True,
             audio_out_enabled=True,
             camera_out_enabled=False,
@@ -58,7 +57,7 @@ async def main(room_url: str, token: str, callId: str, callDomain: str):
     messages = [
         {
             "role": "system",
-            "content": "You are Chatbot, a friendly, helpful robot. Your goal is to demonstrate your capabilities in a succinct way. Your output will be converted to audio so don't include special characters in your answers. Respond to what the user said in a creative and helpful way, but keep your responses brief. Start by saying 'Oh, hello! Who dares dial me at this hour?!'.",
+            "content": "You are Chatbot, a friendly, helpful robot. Your goal is to demonstrate your capabilities in a succinct way. Your output will be converted to audio so don't include special characters in your answers. Respond to what the user said in a creative and helpful way, but keep your responses brief. Start by saying 'Oh, hello! I'm a friendly chatbot. How can I help you?'.",
         },
     ]
 
@@ -78,10 +77,41 @@ async def main(room_url: str, token: str, callId: str, callDomain: str):
 
     task = PipelineTask(pipeline, PipelineParams(allow_interruptions=True))
 
-    @transport.event_handler("on_first_participant_joined")
-    async def on_first_participant_joined(transport, participant):
-        await transport.capture_participant_transcription(participant["id"])
-        await task.queue_frames([context_aggregator.user().get_context_frame()])
+    if dialout_number:
+        logger.debug("dialout number detected; doing dialout")
+
+        # Configure some handlers for dialing out
+        @transport.event_handler("on_joined")
+        async def on_joined(transport, data):
+            logger.debug(f"Joined; starting dialout to: {dialout_number}")
+            await transport.start_dialout({"phoneNumber": dialout_number})
+
+        @transport.event_handler("on_dialout_connected")
+        async def on_dialout_connected(transport, data):
+            logger.debug(f"Dial-out connected: {data}")
+
+        @transport.event_handler("on_dialout_answered")
+        async def on_dialout_answered(transport, data):
+            logger.debug(f"Dial-out answered: {data}")
+
+        @transport.event_handler("on_first_participant_joined")
+        async def on_first_participant_joined(transport, participant):
+            await transport.capture_participant_transcription(participant["id"])
+            # unlike the dialin case, for the dialout case, the caller will speak first. Presumably
+            # they will answer the phone and say "Hello?" Since we've captured their transcript,
+            # That will put a frame into the pipeline and prompt an LLM completion, which is how the
+            # bot will then greet the user.
+
+    else:
+        logger.debug("no dialout number; assuming dialin")
+
+        # Different handlers for dialin
+        @transport.event_handler("on_first_participant_joined")
+        async def on_first_participant_joined(transport, participant):
+            await transport.capture_participant_transcription(participant["id"])
+            # For the dialin case, we want the bot to answer the phone and greet the user. We
+            # can prompt the bot to speak by putting the context into the pipeline.
+            await task.queue_frames([context_aggregator.user().get_context_frame()])
 
     @transport.event_handler("on_participant_left")
     async def on_participant_left(transport, participant, reason):
@@ -98,6 +128,7 @@ if __name__ == "__main__":
     parser.add_argument("-t", type=str, help="Token")
     parser.add_argument("-i", type=str, help="Call ID")
     parser.add_argument("-d", type=str, help="Call Domain")
+    parser.add_argument("-o", type=str, help="Dialout number", default=None)
     config = parser.parse_args()
 
-    asyncio.run(main(config.u, config.t, config.i, config.d))
+    asyncio.run(main(config.u, config.t, config.i, config.d, config.o))
--- examples/phone-chatbot/bot_runner.py
+++ examples/phone-chatbot/bot_runner.py
@@ -73,16 +73,20 @@ action using the Twilio Client library.
 """
 
 
-async def _create_daily_room(room_url, callId, callDomain=None, vendor="daily"):
+async def _create_daily_room(room_url, callId, callDomain=None, dialoutNumber=None, vendor="daily"):
     if not room_url:
-        params = DailyRoomParams(
-            properties=DailyRoomProperties(
-                # Note: these are the default values, except for the display name
-                sip=DailyRoomSipParams(
-                    display_name="dialin-user", video=False, sip_mode="dial-in", num_endpoints=1
-                )
+        # Create base properties with SIP settings
+        properties = DailyRoomProperties(
+            sip=DailyRoomSipParams(
+                display_name="dialin-user", video=False, sip_mode="dial-in", num_endpoints=1
             )
         )
+
+        # Only enable dialout if dialoutNumber is provided
+        if dialoutNumber:
+            properties.enable_dialout = True
+
+        params = DailyRoomParams(properties=properties)
 
         print(f"Creating new room...")
         room: DailyRoomObject = await daily_helpers["rest"].create_room(params=params)
@@ -90,7 +94,6 @@ async def _create_daily_room(room_url, callId, callDomain=None, vendor="daily"):
     else:
         # Check passed room URL exist (we assume that it already has a sip set up!)
         try:
-            print(f"Joining existing room: {room_url}")
             room: DailyRoomObject = await daily_helpers["rest"].get_room_from_url(room_url)
         except Exception:
             raise HTTPException(status_code=500, detail=f"Room not found: {room_url}")
@@ -107,6 +110,8 @@ async def _create_daily_room(room_url, callId, callDomain=None, vendor="daily"):
     # Note: this is mostly for demonstration purposes (refer to 'deployment' in docs)
     if vendor == "daily":
         bot_proc = f"python3 -m bot_daily -u {room.url} -t {token} -i {callId} -d {callDomain}"
+        if dialoutNumber:
+            bot_proc += f" -o {dialoutNumber}"
     else:
         bot_proc = f"python3 -m bot_twilio -u {room.url} -t {token} -i {callId} -s {room.config.sip_endpoint}"
 
@@ -179,11 +184,15 @@ async def daily_start_bot(request: Request) -> JSONResponse:
             return JSONResponse({"test": True})
         callId = data.get("callId", None)
         callDomain = data.get("callDomain", None)
+        dialoutNumber = data.get("dialoutNumber", None)
     except Exception:
-        raise HTTPException(status_code=500, detail="Missing properties 'callId' or 'callDomain'")
+        raise HTTPException(
+            status_code=500, detail="Missing properties 'callId', 'callDomain', or 'dialoutNumber'"
+        )
 
-    print(f"CallId: {callId}, CallDomain: {callDomain}")
-    room: DailyRoomObject = await _create_daily_room(room_url, callId, callDomain, "daily")
+    room: DailyRoomObject = await _create_daily_room(
+        room_url, callId, callDomain, dialoutNumber, "daily"
+    )
 
     # Grab a token for the user to join with
     return JSONResponse({"room_url": room.url, "sipUri": room.config.sip_endpoint})
