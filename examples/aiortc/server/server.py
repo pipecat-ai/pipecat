5869c52dd4fe0d5d1c21a1cf51c30b501f9b0bc2
Filipi Fuchter <filipi87@gmail.com>
2025-02-26 11:20:09 -0300

    Removing code not needed.

--- examples/aiortc/server/server.py
+++ examples/aiortc/server/server.py
@@ -5,11 +5,9 @@ import os
 import uuid
 from contextlib import asynccontextmanager
 
-import cv2
-from fastapi import FastAPI
-from aiortc import MediaStreamTrack, RTCPeerConnection, RTCSessionDescription
-from aiortc.contrib.media import MediaBlackhole, MediaPlayer, MediaRecorder, MediaRelay
-from av import VideoFrame
+from fastapi import FastAPI, BackgroundTasks
+from aiortc import RTCPeerConnection, RTCSessionDescription
+from aiortc.contrib.media import MediaBlackhole, MediaPlayer, MediaRelay
 
 ROOT = os.path.dirname(__file__)
 
@@ -17,76 +15,7 @@ logger = logging.getLogger("pc")
 pcs = set()
 relay = MediaRelay()
 
-# FastAPI instance
 app = FastAPI()
-
-class VideoTransformTrack(MediaStreamTrack):
-    """
-    A video stream track that transforms frames from another track.
-    """
-    kind = "video"
-
-    def __init__(self, track, transform):
-        super().__init__()
-        self.track = track
-        self.transform = transform
-
-    async def recv(self):
-        frame = await self.track.recv()
-
-        if self.transform == "cartoon":
-            img = frame.to_ndarray(format="bgr24")
-
-            # prepare color
-            img_color = cv2.pyrDown(cv2.pyrDown(img))
-            for _ in range(6):
-                img_color = cv2.bilateralFilter(img_color, 9, 9, 7)
-            img_color = cv2.pyrUp(cv2.pyrUp(img_color))
-
-            # prepare edges
-            img_edges = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-            img_edges = cv2.adaptiveThreshold(
-                cv2.medianBlur(img_edges, 7),
-                255,
-                cv2.ADAPTIVE_THRESH_MEAN_C,
-                cv2.THRESH_BINARY,
-                9,
-                2,
-            )
-            img_edges = cv2.cvtColor(img_edges, cv2.COLOR_GRAY2RGB)
-
-            # combine color and edges
-            img = cv2.bitwise_and(img_color, img_edges)
-
-            # rebuild a VideoFrame, preserving timing information
-            new_frame = VideoFrame.from_ndarray(img, format="bgr24")
-            new_frame.pts = frame.pts
-            new_frame.time_base = frame.time_base
-            return new_frame
-        elif self.transform == "edges":
-            # perform edge detection
-            img = frame.to_ndarray(format="bgr24")
-            img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
-
-            # rebuild a VideoFrame, preserving timing information
-            new_frame = VideoFrame.from_ndarray(img, format="bgr24")
-            new_frame.pts = frame.pts
-            new_frame.time_base = frame.time_base
-            return new_frame
-        elif self.transform == "rotate":
-            # rotate image
-            img = frame.to_ndarray(format="bgr24")
-            rows, cols, _ = img.shape
-            M = cv2.getRotationMatrix2D((cols / 2, rows / 2), frame.time * 45, 1)
-            img = cv2.warpAffine(img, M, (cols, rows))
-
-            # rebuild a VideoFrame, preserving timing information
-            new_frame = VideoFrame.from_ndarray(img, format="bgr24")
-            new_frame.pts = frame.pts
-            new_frame.time_base = frame.time_base
-            return new_frame
-        else:
-            return frame
 
 
 class WebRTCConnection:
@@ -133,9 +62,8 @@ class WebRTCConnection:
                 pc.addTrack(player.audio)
                 recorder.addTrack(track)
             elif track.kind == "video":
-                pc.addTrack(
-                    VideoTransformTrack(relay.subscribe(track), transform=self.transform)
-                )
+                # TODO: we probably don't need the mediarelay here, but keeping it here for now
+                pc.addTrack(relay.subscribe(track))
 
             @track.on("ended")
             async def on_ended():
@@ -159,7 +87,7 @@ class WebRTCConnection:
 
 
 @app.post("/api/offer")
-async def offer(request: dict):
+async def offer(request: dict, background_tasks: BackgroundTasks):
     params = request
     offer = RTCSessionDescription(sdp=params["sdp"], type=params["type"])
 
@@ -170,10 +98,18 @@ async def offer(request: dict):
     webrtc_connection = WebRTCConnection(transform)
     pc = await webrtc_connection.create_peer_connection(offer)
 
+    # This background task will run after sending the response
+    background_tasks.add_task(run_additional_logic, webrtc_connection)
+
+    # Send the answer back to the client
     return {
         "sdp": pc.localDescription.sdp,
         "type": pc.localDescription.type
     }
+
+
+async def run_additional_logic(webrtc_connection: WebRTCConnection):
+    logger.info("Should run the bot here passing the peer connection")
 
 
 @asynccontextmanager
