f6f0cd51163de3b529c44b18deb3dfc40d95e94f
Filipi Fuchter <filipi87@gmail.com>
2025-03-06 10:34:25 -0300

    Adding missing flush_audio method to AzureTTSService.

--- src/pipecat/services/azure.py
+++ src/pipecat/services/azure.py
@@ -577,6 +577,10 @@ class AzureTTSService(AzureBaseTTSService):
         logger.error(f"Speech synthesis canceled: {evt.result.cancellation_details.reason}")
         self._audio_queue.put_nowait(None)
 
+    async def flush_audio(self):
+        logger.trace(f"{self}: flushing audio")
+        # TODO: check what we need to implement here ?
+
     async def run_tts(self, text: str) -> AsyncGenerator[Frame, None]:
         logger.debug(f"{self}: Generating TTS [{text}]")
 
