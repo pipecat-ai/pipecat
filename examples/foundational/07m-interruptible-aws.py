94625a253562733376ebe77f9ead869c530cee9c
Adithya Suresh <adxthya@amazon.com>
2025-04-04 05:39:08 +0000

    Restructured STT and enabled prosody tags for generative Polly

--- examples/foundational/07m-interruptible-aws.py
+++ examples/foundational/07m-interruptible-aws.py
@@ -17,9 +17,9 @@ from pipecat.audio.vad.silero import SileroVADAnalyzer
 from pipecat.pipeline.pipeline import Pipeline
 from pipecat.pipeline.runner import PipelineRunner
 from pipecat.pipeline.task import PipelineParams, PipelineTask
-from pipecat.processors.aggregators.openai_llm_context import OpenAILLMContext
-from pipecat.services.aws import PollyTTSService, TranscribeSTTService
-from pipecat.services.openai import OpenAILLMService
+from pipecat.services.aws.llm import BedrockLLMService, BedrockLLMContext
+from pipecat.services.aws.stt import TranscribeSTTService
+from pipecat.services.aws.tts import PollyTTSService
 from pipecat.transports.services.daily import DailyParams, DailyTransport
 
 load_dotenv(override=True)
@@ -49,19 +49,30 @@ async def main():
         tts = PollyTTSService(
             region="us-west-2",  # only specific regions support generative TTS
             voice_id="Joanna",
-            params=PollyTTSService.InputParams(engine="generative", language="en-US", rate="1.05"),
+            params=PollyTTSService.InputParams(
+                engine="generative", 
+                language="en-US", 
+                rate="1.1"
+            ),
         )
 
-        llm = OpenAILLMService(api_key=os.getenv("OPENAI_API_KEY"), model="gpt-4o")
+        llm = BedrockLLMService(
+            aws_region="us-west-2",
+            model="us.anthropic.claude-3-5-haiku-20241022-v1:0",
+            params=BedrockLLMService.InputParams(
+                temperature=0.8,
+                latency="optimized"
+            )
+        )
 
         messages = [
             {
                 "role": "system",
-                "content": "You are a helpful LLM in a WebRTC call. Your goal is to demonstrate your capabilities in a succinct way. Your output will be converted to audio so don't include special characters in your answers. Respond to what the user said in a creative and helpful way.",
+                "content": [{"text": "You are a helpful LLM in a WebRTC call. Your goal is to demonstrate your capabilities in a succinct way. Your output will be converted to audio so don't include special characters in your answers. Respond to what the user said in a creative and helpful way."}],
             },
         ]
 
-        context = OpenAILLMContext(messages)
+        context = BedrockLLMContext(messages)
         context_aggregator = llm.create_context_aggregator(context)
 
         pipeline = Pipeline(
@@ -90,7 +101,7 @@ async def main():
         async def on_first_participant_joined(transport, participant):
             await transport.capture_participant_transcription(participant["id"])
             # Kick off the conversation.
-            messages.append({"role": "system", "content": "Please introduce yourself to the user."})
+            messages.append({"role": "user", "content": [{"text": "Please introduce yourself to the user."}]})
             await task.queue_frames([context_aggregator.user().get_context_frame()])
 
         @transport.event_handler("on_participant_left")
