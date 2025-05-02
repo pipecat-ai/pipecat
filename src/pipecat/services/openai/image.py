0c541fc5d514eb64e86ba5416ff28b037a5a56bd
Óscar Carrasco <me@oxca.be>
2025-05-02 23:31:09 +0100

    refactor: add modality based constructor for `OpenAIImageGenService`
    In order to avoid making any breaking changes, both constructors now
    overload into a single, general implementation that handles the params
    accordingly.
    
    The former constructor is now set as deprecated. Ideally, it should
    be removed as part of the next major version release.
    
    If a date or event for removal is set, the deprecation message
    should then be updated to provide this information to the framework
    users.

--- src/pipecat/services/openai/image.py
+++ src/pipecat/services/openai/image.py
@@ -5,11 +5,19 @@
 #
 
 import io
-from typing import AsyncGenerator, Literal, Optional
+from functools import partial
+from sys import version_info
+from typing import Any, AsyncGenerator, Literal, Mapping, Optional, overload
+
+if version_info < (3, 13):
+    from typing_extensions import deprecated
+else:
+    from warnings import deprecated
 
 import aiohttp
 from loguru import logger
 from openai import AsyncOpenAI
+from openai.types import ImageModel
 from PIL import Image
 
 from pipecat.frames.frames import (
@@ -21,28 +29,80 @@ from pipecat.services.image_service import ImageGenService
 
 
 class OpenAIImageGenService(ImageGenService):
+    Modality = Literal["generate", "edit", "create_variation"]
+    InputParams = Mapping[str, Any]
+
+    @overload
+    @deprecated('Use `params["image_size"]` to set the image size instead')
+    def __init__(
+        self,
+        *,
+        api_key: str,
+        base_url: Optional[str],
+        aiohttp_session: aiohttp.ClientSession,
+        image_size: Literal["256x256", "512x512", "1024x1024", "1792x1024", "1024x1792"],
+        model: Optional[str],
+    ): ...
+
+    @overload
+    def __init__(
+        self,
+        *,
+        api_key: str,
+        base_url: Optional[str],
+        aiohttp_session: aiohttp.ClientSession,
+        model: Optional[ImageModel],
+        modality: Optional[Literal["generate"]],
+        params: Optional[InputParams],
+        **kwargs,
+    ): ...
+
     def __init__(
         self,
         *,
         api_key: str,
         base_url: Optional[str] = None,
         aiohttp_session: aiohttp.ClientSession,
-        image_size: Literal["256x256", "512x512", "1024x1024", "1792x1024", "1024x1792"],
+        modality: Modality = "generate",
+        image_size: Optional[str] = None,
         model: str = "dall-e-3",
+        params: InputParams = dict(),
+        **kwargs,
     ):
-        super().__init__()
+        # Fail fast if `params` contains any of `model` or `n` keys,
+        # as it would override such params when instantiating
+        # `self._generate_image` down below.
+        # The value for `n` is forced to be `1` for now, since there's
+        # no support for generating multiple images yet.
+        # The value for `model` should be passed as a top-level method
+        # argument instead.
+        invalid_params = [param for param in ["model", "n"] if param in params]
+        if invalid_params:
+            raise ValueError(
+                f"Protected parameter(s) {', '.join(invalid_params)} cannot be specified inside params."
+            )
+
+        # Temporary guard until the remaining modalities are implemented
+        if modality != "generate":
+            raise ValueError(f"Unimplemented or unknown modality '{modality}'")
+
+        super().__init__(**kwargs)
         self.set_model_name(model)
-        self._image_size = image_size
-        self._client = AsyncOpenAI(api_key=api_key, base_url=base_url)
         self._aiohttp_session = aiohttp_session
+
+        client = AsyncOpenAI(api_key=api_key, base_url=base_url)
+        self._generate_image = partial(
+            client.images.generate,
+            model=self.model_name,
+            n=1,
+            size=image_size,
+            **params,
+        )
 
     async def run_image_gen(self, prompt: str) -> AsyncGenerator[Frame, None]:
         logger.debug(f"Generating image from prompt: {prompt}")
 
-        image = await self._client.images.generate(
-            prompt=prompt, model=self.model_name, n=1, size=self._image_size
-        )
-
+        image = await self._generate_image(prompt=prompt)
         image_url = image.data[0].url
 
         if not image_url:
