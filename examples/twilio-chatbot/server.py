c6a6e3b544507c42d8ffc904b62257ad3262786b
Hitesh Wadhwani <hitesh@smallest.ai>
2025-05-25 18:32:00 +0530

    added recording status webhook

--- examples/twilio-chatbot/server.py
+++ examples/twilio-chatbot/server.py
@@ -1,66 +1,3 @@
-# #
-# # Copyright (c) 2025, Daily
-# #
-# # SPDX-License-Identifier: BSD 2-Clause License
-# #
-
-# import argparse
-# import json
-
-# import uvicorn
-# from bot import run_bot
-# from fastapi import FastAPI, WebSocket
-# from fastapi.middleware.cors import CORSMiddleware
-# from starlette.responses import HTMLResponse
-
-# app = FastAPI()
-
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],  # Allow all origins for testing
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-
-
-# @app.post("/")
-# async def start_call():
-#     print("POST TwiML")
-#     return HTMLResponse(content=open("templates/streams.xml").read(), media_type="application/xml")
-
-
-# @app.websocket("/ws")
-# async def websocket_endpoint(websocket: WebSocket):
-#     await websocket.accept()
-#     start_data = websocket.iter_text()
-#     await start_data.__anext__()
-#     call_data = json.loads(await start_data.__anext__())
-#     print(call_data, flush=True)
-#     stream_sid = call_data["start"]["streamSid"]
-#     call_sid = call_data["start"]["callSid"]
-#     print("WebSocket connection accepted")
-#     await run_bot(websocket, stream_sid, call_sid, app.state.testing)
-
-
-# if __name__ == "__main__":
-#     parser = argparse.ArgumentParser(description="Pipecat Twilio Chatbot Server")
-#     parser.add_argument(
-#         "-t", "--test", action="store_true", default=False, help="set the server in testing mode"
-#     )
-#     args, _ = parser.parse_known_args()
-
-#     app.state.testing = args.test
-
-#     uvicorn.run(app, host="0.0.0.0", port=8765)
-
-
-#
-# Copyright (c) 2025, Daily
-#
-# SPDX-License-Identifier: BSD 2-Clause License
-#
-
 import argparse
 import json
 import os
@@ -71,6 +8,7 @@ from bot import run_bot
 from fastapi import FastAPI, Request, Response, WebSocket
 from fastapi.middleware.cors import CORSMiddleware
 from jinja2 import Environment, FileSystemLoader
+from loguru import logger
 from starlette.responses import HTMLResponse
 
 app = FastAPI()
@@ -131,6 +69,8 @@ async def outbound_call(request: Request):
         "Twiml": get_connection_twiml(DEFAULT_TEMPLATE_ENVIRONMENT).body.decode("utf-8"),
         "To": f"{to}",
         "From": f"+18287528082",
+        "Record": True,
+        "RecordingStatusCallback": f"https://{os.getenv('SERVER_BASE_URL')}/webhook/twilio/recording_status",
     }
     basic_auth = httpx.BasicAuth(
         username=os.getenv("TWILIO_ACCOUNT_SID"),
@@ -150,6 +90,22 @@ async def outbound_call(request: Request):
         #         print(await response.json())
         # response = await response.json()
         # return response["sid"]
+
+
+@app.post("/webhook/twilio/recording_status")
+async def handle_twilio_recording_status_callback(request: Request):
+    """This function will handle the twilio recording status callback."""
+    try:
+        form = await request.form()
+        call_data = dict(form)
+
+        call_sid = call_data["CallSid"]
+        recording_url = call_data["RecordingUrl"]
+
+        logger.info(f"call_sid: {call_sid}, recording_url: {recording_url}")
+
+    except Exception as e:
+        logger.error(f"Failed to handle twilio recording status callback: {str(e)}", exc_info=True)
 
 
 if __name__ == "__main__":
