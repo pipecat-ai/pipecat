0073a868d4916a440698bb3e503af4da633a0cbc
Filipi Fuchter <filipi87@gmail.com>
2025-06-10 11:34:02 -0300

    Websocket client web app to test Twilio.

---
+++ examples/twilio-chatbot/ws_test_client/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+export default defineConfig({
+    plugins: [react()],
+    server: {
+        proxy: {
+            // Proxy /api requests to the backend server
+            '/ws': {
+                target: 'http://0.0.0.0:8765', // Replace with your backend URL
+                changeOrigin: true,
+            },
+        },
+    },
+});
