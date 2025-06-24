a4e6ea5a3fea03a3d4aa3313d745ab34756a8585
Aleix Conchillo Flaqué <aleix@daily.co>
2025-06-24 11:27:39 -0700

    HeartbeatFrames are now control frames

--- src/pipecat/frames/frames.py
+++ src/pipecat/frames/frames.py
@@ -486,16 +486,6 @@ class FatalErrorFrame(ErrorFrame):
 
 
 @dataclass
-class HeartbeatFrame(SystemFrame):
-    """This frame is used by the pipeline task as a mechanism to know if the
-    pipeline is running properly.
-
-    """
-
-    timestamp: int
-
-
-@dataclass
 class EndTaskFrame(SystemFrame):
     """This is used to notify the pipeline task that the pipeline should be
     closed nicely (flushing all the queued frames) by pushing an EndFrame
@@ -875,6 +865,16 @@ class StopFrame(ControlFrame):
     """
 
     pass
+
+
+@dataclass
+class HeartbeatFrame(ControlFrame):
+    """This frame is used by the pipeline task as a mechanism to know if the
+    pipeline is running properly.
+
+    """
+
+    timestamp: int
 
 
 @dataclass
