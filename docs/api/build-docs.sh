b6637b7932cd64e70c59a2622ec7c4a561f31fd0
Mark Backman <mark@daily.co>
2025-07-27 08:16:05 -0400

    Add pipecat.runner to docs auto-generation

--- docs/api/build-docs.sh
+++ docs/api/build-docs.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Check if sphinx-build is installed
+if ! command -v sphinx-build &> /dev/null; then
+    echo "Error: sphinx-build is not installed or not in PATH" >&2
+    echo "Please install Sphinx using: pip install -r requirements.txt" >&2
+    exit 1
+fi
+
 # Clean previous build
 rm -rf _build
 
