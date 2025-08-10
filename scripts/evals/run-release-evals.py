d353907968d0d68cc6b025dafe3ce8fef1f7a07f
Mark Backman <mark@daily.co>
2025-08-10 09:40:54 -0400

    Add foundational 14w-function-calling.py

--- scripts/evals/run-release-evals.py
+++ scripts/evals/run-release-evals.py
@@ -113,6 +113,7 @@ TESTS_14 = [
     ("14q-function-calling-qwen.py", PROMPT_WEATHER, EVAL_WEATHER),
     ("14r-function-calling-aws.py", PROMPT_WEATHER, EVAL_WEATHER),
     ("14v-function-calling-openai.py", PROMPT_WEATHER, EVAL_WEATHER),
+    ("14w-function-calling-mistral.py", PROMPT_WEATHER, EVAL_WEATHER),
     # Currently not working.
     # ("14c-function-calling-together.py", PROMPT_WEATHER, EVAL_WEATHER),
     # ("14k-function-calling-cerebras.py", PROMPT_WEATHER, EVAL_WEATHER),
