8ee28b37cdd31811294ed077c0947a081004fcbc
Aleix Conchillo Flaqué <aleix@daily.co>
2025-10-10 06:41:19 -0700

    google: rename google.gemini_live.vertext to google.gemini_live.llm_vertex

--- examples/foundational/26h-gemini-multimodal-live-vertex.py
+++ examples/foundational/26h-gemini-multimodal-live-vertex.py
@@ -17,7 +17,7 @@ from pipecat.pipeline.runner import PipelineRunner
 from pipecat.pipeline.task import PipelineParams, PipelineTask
 from pipecat.runner.types import RunnerArguments
 from pipecat.runner.utils import create_transport
-from pipecat.services.google.gemini_live.vertex import GeminiLiveVertexLLMService
+from pipecat.services.google.gemini_live.llm_vertex import GeminiLiveVertexLLMService
 from pipecat.transports.base_transport import BaseTransport, TransportParams
 from pipecat.transports.daily.transport import DailyParams
 from pipecat.transports.websocket.fastapi import FastAPIWebsocketParams
