18cec148bf7f9680651a0e09b16a34b9edd344bc
Aleix Conchillo Flaqué <aleix@daily.co>
2025-10-09 19:00:54 -0700

    move openai_realtime to openai.realtime

--- examples/foundational/19b-openai-realtime-text.py
+++ examples/foundational/19b-openai-realtime-text.py
@@ -24,14 +24,15 @@ from pipecat.runner.types import RunnerArguments
 from pipecat.runner.utils import create_transport
 from pipecat.services.cartesia import CartesiaTTSService
 from pipecat.services.llm_service import FunctionCallParams
-from pipecat.services.openai_realtime import (
+from pipecat.services.openai.realtime.events import (
+    AudioConfiguration,
+    AudioInput,
     InputAudioNoiseReduction,
     InputAudioTranscription,
-    OpenAIRealtimeLLMService,
     SemanticTurnDetection,
     SessionProperties,
 )
-from pipecat.services.openai_realtime.events import AudioConfiguration, AudioInput
+from pipecat.services.openai.realtime.openai import OpenAIRealtimeLLMService
 from pipecat.transports.base_transport import BaseTransport, TransportParams
 from pipecat.transports.daily.transport import DailyParams
 from pipecat.transports.websocket.fastapi import FastAPIWebsocketParams
--- src/pipecat/services/openai_realtime/__init__.py
+++
@@ -1,9 +0,0 @@
-from .azure import AzureRealtimeLLMService
-from .events import (
-    InputAudioNoiseReduction,
-    InputAudioTranscription,
-    SemanticTurnDetection,
-    SessionProperties,
-    TurnDetection,
-)
-from .openai import OpenAIRealtimeLLMService
