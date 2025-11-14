e8640d84aecb2b5a72a6a7f8a7f228efddcaf03c
mattie ruth backman <mattieruth@gmail.com>
2025-11-14 17:13:08 -0500

    test fix now that we send an aggregated text frame for non word-by-word tts services

--- tests/test_piper_tts.py
+++ tests/test_piper_tts.py
@@ -75,6 +75,7 @@ async def test_run_piper_tts_success(aiohttp_client):
         ]
 
         expected_returned_frames = [
+            AggregatedTextFrame,
             TTSStartedFrame,
             TTSAudioRawFrame,
             TTSAudioRawFrame,
@@ -122,7 +123,7 @@ async def test_run_piper_tts_error(aiohttp_client):
             TTSSpeakFrame(text="Error case."),
         ]
 
-        expected_down_frames = [TTSStoppedFrame, TTSTextFrame]
+        expected_down_frames = [AggregatedTextFrame, TTSStoppedFrame, TTSTextFrame]
 
         expected_up_frames = [ErrorFrame]
 
