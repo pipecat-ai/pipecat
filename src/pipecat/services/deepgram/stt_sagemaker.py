da0fb1cefe1a5e8db7f55da79520873a26de6190
Mark Backman <mark@daily.co>
2025-12-22 11:02:12 -0500

    Update DeepgramSageMakerSTTService to use VADUser*SpeakingFrame

--- src/pipecat/services/deepgram/stt_sagemaker.py
+++ src/pipecat/services/deepgram/stt_sagemaker.py
@@ -26,8 +26,8 @@ from pipecat.frames.frames import (
     InterimTranscriptionFrame,
     StartFrame,
     TranscriptionFrame,
-    UserStartedSpeakingFrame,
-    UserStoppedSpeakingFrame,
+    VADUserStartedSpeakingFrame,
+    VADUserStoppedSpeakingFrame,
 )
 from pipecat.processors.frame_processor import FrameDirection
 from pipecat.services.aws.sagemaker.bidi_client import SageMakerBidiClient
@@ -432,9 +432,9 @@ class DeepgramSageMakerSTTService(STTService):
         await super().process_frame(frame, direction)
 
         # Start metrics when user starts speaking (if VAD is not provided by Deepgram)
-        if isinstance(frame, UserStartedSpeakingFrame):
+        if isinstance(frame, VADUserStartedSpeakingFrame):
             await self.start_metrics()
-        elif isinstance(frame, UserStoppedSpeakingFrame):
+        elif isinstance(frame, VADUserStoppedSpeakingFrame):
             # Send finalize message to Deepgram when user stops speaking
             # This tells Deepgram to flush any remaining audio and return final results
             if self._client and self._client.is_active:
