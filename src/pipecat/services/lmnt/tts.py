70e081e87b72e539b00ea0da446fbf356b074af7
filipi87 <filipi87@gmail.com>
2026-01-30 09:25:56 -0300

    Refactoring LmntTTSService

--- src/pipecat/services/lmnt/tts.py
+++ src/pipecat/services/lmnt/tts.py
@@ -254,6 +254,9 @@ class LmntTTSService(InterruptibleTTSService):
 
     async def _receive_messages(self):
         """Receive messages from LMNT websocket."""
+        # Note: context_id is not available in this method since it's not passed through WebSocket messages
+        # Audio frames will not have context_id set in this streaming implementation.
+        # But the TTSTextFrame will have the context_id, because they will be pushed by the TTSService
         async for message in self._get_websocket():
             if isinstance(message, bytes):
                 # Raw audio data
