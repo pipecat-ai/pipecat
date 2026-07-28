59cae26504659fea1a6168a040cddb9252299dd7
Mark Backman <mark@daily.co>
2026-07-28 18:09:52 -0400

    Keep the keenable package __init__ import-free like other services

--- src/pipecat/services/keenable/__init__.py
+++ src/pipecat/services/keenable/__init__.py
@@ -3,14 +3,3 @@
 #
 # SPDX-License-Identifier: BSD 2-Clause License
 #
-
-"""Web search integration powered by Keenable AI (https://keenable.ai).
-
-See :mod:`pipecat.services.keenable.search` for the web search wrapper.
-"""
-
-from pipecat.services.keenable.search import KeenableWebSearch
-
-__all__ = [
-    "KeenableWebSearch",
-]
